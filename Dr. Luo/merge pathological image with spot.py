374204174a444c13ef365be5fe166b209bbc7dda
1QiGuo <96271990+1QiGuo@users.noreply.github.com>
2023-04-22 21:49:46 -0400

    Update merge pathological image with spot.py

--- Dr. Luo/merge pathological image with spot.py
+++ Dr. Luo/merge pathological image with spot.py
@@ -1,5 +1,3 @@
-# function
-
 # load package
 import numpy as np
 import pandas as pd
@@ -13,7 +11,7 @@ import matplotlib.colors as clr
 import seaborn as sns
 import math
 
-#function
+# function
 def merge_transformed_RGB_to_spot_and_save(spot_row_in_fullres,
                                           spot_col_in_fullres,
                                           #max_row, max_col,
@@ -46,44 +44,40 @@ def merge_transformed_RGB_to_spot_and_save(spot_row_in_fullres,
     modulescore_scale[modulescore_colname]=modulescore_scale[modulescore_colname]*255/max_score
     for index in range(len(modulescore_scale[modulescore_colname])):
         cv2.circle(cv_object_9_t2, 
-                   (spot_col_in_fullres.iloc[index], 
-                    spot_row_in_fullres.iloc[index]),
-                    plot_spot_radius,
-                    color=(int(modulescore_scale[modulescore_colname].iloc[index]), 0, 255),
-                    thickness=-1)
+                   (spot_col_in_fullres.iloc[index], spot_row_in_fullres.iloc[index]),
+                      plot_spot_radius,
+                      color=(125,int(modulescore_scale[modulescore_colname].iloc[index]), 125),
+                      thickness=-1)
     #add transparancy
-    alpha = 0.3
+    alpha = 0.4
     cv_object_f = cv2.addWeighted(cv_object_9_t2, alpha, overlay, 1 - alpha, 0)
     #export
     cv2.imwrite(result_path+sample_name + '_merged.tiff', cv2.cvtColor(cv_object_f, cv2.COLOR_RGB2BGR))
     #add figure legend
     fig, ax = plt.subplots()
-    cmap = clr.LinearSegmentedColormap.from_list('custom blue', ['#0000ff','#ff00ff'], N=256)
+    cmap = clr.LinearSegmentedColormap.from_list('custom blue', ['#7d007d','#7dff7d'], N=256)
     img = ax.imshow(cv_object_f, 
-                    vmin=modulescore[modulescore_colname].min(), 
-                    vmax=modulescore[modulescore_colname].max(), 
-                    cmap=cmap)
+                vmin=modulescore[modulescore_colname].min(), 
+                vmax=modulescore[modulescore_colname].max(), 
+                cmap=cmap)
     cbar = fig.colorbar(img)
     cbar.ax.set_ylabel('Label', rotation=90)
     plt.savefig(result_path+sample_name + '_merged_colorbar.pdf')
-
     
-# output
-## 2-3 as example
+#output
 
-result_path="/bmbl_data/qiguo/AD/wenjie/result_Apr16"
+result_path="/bmbl_data/qiguo/AD/wenjie/result_Apr17_changespotsize/2_3"
 imagepath="/bmbl_data/qiguo/AD/AD_home/spatial_rawdata/spatial_8/image/stack/B1 2-3 Stack.tif"
+#plot 2-3 spot plot using tissue file, json (diameter), and module score
 tissue_positioin_23 = pd.read_csv('/bmbl_data/qiguo/AD/AD_home/spatial_rawdata/spatial_8/image/2_3/2_3_tissue_position_filtered.csv', 
                                   delimiter = ',',
                                   index_col=0)
 modulescore_23 = pd.read_csv('/bmbl_data/qiguo/AD/AD_home/spatial_rawdata/spatial_8/image/2_3/2_3_modulescore.csv', 
                              delimiter = ',',
                              index_col=0)
-diameter2_3=math.ceil(232.34944000000002*0.5)                         
-
-#plot 2-3 spot plot using tissue file, json (diameter), and module score
+diameter2_3=round(143.83536*0.5)
 #2_3 geneset1
-module_scale=merge_transformed_RGB_to_spot_and_save(tissue_positioin_23["pxl_row_in_fullres"],
+merge_transformed_RGB_to_spot_and_save(tissue_positioin_23["pxl_row_in_fullres"],
                                        tissue_positioin_23["pxl_col_in_fullres"],
                                        imagepath,
                                        modulescore_23,
@@ -93,7 +87,7 @@ module_scale=merge_transformed_RGB_to_spot_and_save(tissue_positioin_23["pxl_row
                                        diameter2_3#should be integer instead of float
                                          )
 #2_3 geneset2
-module_scale=merge_transformed_RGB_to_spot_and_save(tissue_positioin_23["pxl_row_in_fullres"],
+merge_transformed_RGB_to_spot_and_save(tissue_positioin_23["pxl_row_in_fullres"],
                                        tissue_positioin_23["pxl_col_in_fullres"],
                                        imagepath,
                                        modulescore_23,
@@ -102,7 +96,7 @@ module_scale=merge_transformed_RGB_to_spot_and_save(tissue_positioin_23["pxl_row
                                        "2_3_geneset2",
                                        diameter2_3#should be integer instead of float
                                          )
-#2_3 geneset1                                        
+#geneset3
 module_scale=merge_transformed_RGB_to_spot_and_save(tissue_positioin_23["pxl_row_in_fullres"],
                                        tissue_positioin_23["pxl_col_in_fullres"],
                                        imagepath,
@@ -112,6 +106,3 @@ module_scale=merge_transformed_RGB_to_spot_and_save(tissue_positioin_23["pxl_row
                                        "2_3_geneset3",
                                        diameter2_3#should be integer instead of float
                                          )
-
-
-
