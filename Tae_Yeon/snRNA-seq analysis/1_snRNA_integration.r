90ad430efc9bee0ca10166f47362609e7357e5c8
Qi Guo <96271990+1QiGuo@users.noreply.github.com>
2026-01-15 16:43:33 -0500

    Update 1_snRNA_integration.r

--- Tae_Yeon/snRNA-seq analysis/1_snRNA_integration.r
+++ Tae_Yeon/snRNA-seq analysis/1_snRNA_integration.r
@@ -1,47 +1,72 @@
-#load packages
+#Integrate six RNA samples
+library(qs)
+library("Seurat")
+set.seed(1234)
 dyn.load("/apps/hdf5-serial/gnu/9.1/1.12.0/lib/libhdf5_hl.so.200")
 library(hdf5r)
-library(Seurat)
-library(qs)
-library(ggplot2)
-setwd("/fs/ess/PCON0022/guoqi/NC-snrna/codes")
-
-#load data
+#load data from OSC
 setwd("/fs/ess/PCON0022/Yuzhou/Shane_ATAC/Primary_data_processing")
 all_file_name <- list.files()
+all_file_name<-all_file_name[-c(2,4)]
 object <- list()
-for(i in 1:8){
+for(i in 1:6){
   a <-
     Read10X_h5(paste0(all_file_name[i], "/filtered_feature_bc_matrix.h5"))
   x <-
-    CreateSeuratObject(counts = a[[1]],project =strsplit(all_file_name[i],"_")[[1]][1] ,min.cells = 3, min.features = 200)
+    CreateSeuratObject(counts = a[[1]],project =strsplit(all_file_name[i],"_",)[[1]][1] ,min.cells = 3, min.features = 200)
   x[["percent.mt"]] <- PercentageFeatureSet(x, pattern = "^MT-")
   object[i]<-x
 }
 names(object)<-all_file_name
+setwd("/fs/ess/PCON0022/guoqi/NC/Revision/Output")
+qsave(object,"rna6objects_filter_preqc.qs")
+#annotation
+object[[1]]$orig.ident<-"1-1"
+object[[2]]$orig.ident<-"18-64"
+object[[3]]$orig.ident<-"2-3"
+object[[4]]$orig.ident<-"2-5"
+object[[5]]$orig.ident<-"2-8"
+object[[6]]$orig.ident<-"T4857"
+
+object[[1]]$stage<-"Control"
+object[[2]]$stage<-"Control"
+object[[3]]$stage<-"Mid-AD"
+object[[4]]$stage<-"Control"
+object[[5]]$stage<-"Mid-AD"
+object[[6]]$stage<-"Mid-AD"
+
+object[[1]]$condition<-"Control"
+object[[2]]$condition<-"Control"
+object[[3]]$condition<-"AD"
+object[[4]]$condition<-"Control"
+object[[5]]$condition<-"AD"
+object[[6]]$condition<-"AD"
+#QC
 object_qc<-list()
-object_qc[[1]] <- subset(object[[1]], subset = nFeature_RNA > 200 & nFeature_RNA < 10000 & percent.mt < 10)
-object_qc[[2]] <- subset(object[[2]], subset = nFeature_RNA > 200 & nFeature_RNA < 7000 & percent.mt < 10)
-object_qc[[3]] <- subset(object[[3]], subset = nFeature_RNA > 200 & nFeature_RNA < 10000 & percent.mt < 10)
-object_qc[[4]] <- subset(object[[4]], subset = nFeature_RNA > 200 & nFeature_RNA < 11000 & percent.mt < 10)
-object_qc[[5]] <- subset(object[[5]], subset = nFeature_RNA > 200 & nFeature_RNA < 8000 & percent.mt < 10)
-object_qc[[6]] <- subset(object[[6]], subset = nFeature_RNA > 200 & nFeature_RNA < 10000 & percent.mt < 10)
-object_qc[[7]] <- subset(object[[7]], subset = nFeature_RNA > 200 & nFeature_RNA < 9000 & percent.mt < 10)
-object_qc[[8]] <- subset(object[[8]], subset = nFeature_RNA > 200 & nFeature_RNA < 10000 & percent.mt < 10)
-object_pre_mer<-merge(object_qc[[1]],list(object_qc[[2]],object_qc[[3]],object_qc[[4]],object_qc[[5]],object_qc[[6]],object_qc[[7]],object_qc[[8]]),
-                      add.cell.ids=c("1_1","1_7","18_64","2_10","2_3","2_5","2_8","T4857"))
-object_list<-SplitObject(object_pre_mer,split.by = "orig.ident")
-object_list <- lapply(X = object_list, FUN = SCTransform)
-features <- SelectIntegrationFeatures(object.list = object_list, nfeatures = 3000)
-object_list <- PrepSCTIntegration(object.list = object_list, anchor.features = features)
-anchors <- FindIntegrationAnchors(object.list = object_list, normalization.method = "SCT",
+VlnPlot(object[[1]], features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+object_qc[[1]] <- subset(object[[1]], subset = nFeature_RNA > 200 & nFeature_RNA < 10000 & percent.mt < 15)
+VlnPlot(object[[2]], features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+object_qc[[2]] <- subset(object[[2]], subset = nFeature_RNA > 200 & nFeature_RNA < 12000 & percent.mt < 15)
+VlnPlot(object[[3]], features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+object_qc[[3]] <- subset(object[[3]], subset = nFeature_RNA > 200 & nFeature_RNA < 10000 & percent.mt < 15)
+VlnPlot(object[[4]], features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+object_qc[[4]] <- subset(object[[4]], subset = nFeature_RNA > 200 & nFeature_RNA < 10000 & percent.mt < 15)
+VlnPlot(object[[5]], features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+object_qc[[5]] <- subset(object[[5]], subset = nFeature_RNA > 200 & nFeature_RNA < 10000 & percent.mt < 15)
+VlnPlot(object[[6]], features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+object_qc[[6]] <- subset(object[[6]], subset = nFeature_RNA > 200 & nFeature_RNA < 10000 & percent.mt < 15)
+#Normalization
+object_qc<-lapply(object_qc,function(x){RenameCells(x,add.cell.id = x$orig.ident[1])})
+object_sct<-lapply(object_qc,SCTransform)
+qsave(object_sct,"rna6_sct.qs")
+#Integration
+features <- SelectIntegrationFeatures(object.list = object_sct, nfeatures = 3000)
+object_sct <- PrepSCTIntegration(object.list = object_sct, anchor.features = features)
+anchors <- FindIntegrationAnchors(object.list = object_sct, normalization.method = "SCT",
                                   anchor.features = features)
 object.int <- IntegrateData(anchorset = anchors, normalization.method = "SCT")
 object.int <- RunPCA(object.int, verbose = FALSE)
 object.int <- RunUMAP(object.int, reduction = "pca", dims = 1:30)
-object.int <- FindNeighbors(object.int, reduction = "pca", dims = 1:30)
-object.int <- FindClusters(object.int, resolution = 0.01)
-p1 <- DimPlot(object.int, reduction = "umap", group.by = "orig.ident")
-p2 <- DimPlot(object.int, reduction = "umap", ,group.by = "seurat_clusters",label = TRUE, repel = TRUE)
-p1+p2
-qsave(object.int,"/fs/ess/PCON0022/guoqi/NC-snrna/atac_output/new_rna_integration.qs")
+DimPlot(object.int, reduction = "umap", group.by = "orig.ident")
+qsave(object.int,"rna6_integration.qs")
+
